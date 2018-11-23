73a36c9e4d56ac6386c75cf4d1d098b3f881cd74
Asko Krimann <askokrimann@hotmail.com>
2018-11-23 10:10:34 +0200

    V2

--- app/src/main/java/com/example/askok/ringerservice/MainActivity.java
+++ app/src/main/java/com/example/askok/ringerservice/MainActivity.java
@@ -1,36 +1,77 @@
 package com.example.askok.ringerservice;
 
-import android.annotation.SuppressLint;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
+
 import android.media.MediaPlayer;
 import android.os.Handler;
+
+import android.provider.MediaStore;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.Set;
 
-    Context context = this;
+
+public class MainActivity extends AppCompatActivity  {
+
+
     Button bstart, bstop;
+    private MediaPlayer mp;
+
+    private RingService mSensorService;
+    Intent mServiceIntent;
+
+    Context ctx;
+
+    public Context getCtx() {
+        return ctx;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ctx = this;
+        mSensorService = new RingService(getCtx());
+        mServiceIntent = new Intent(getCtx(), mSensorService.getClass());
+        if (!isMyServiceRunning(mSensorService.getClass())) {
+            startService(mServiceIntent);
+        }
+
+        //mp = MediaPlayer.create(this, Settings.System.DEFAULT_RINGTONE_URI);
+
         bstart = findViewById(R.id.btnstart);
         bstop = findViewById(R.id.btnstop);
 
-        bstart.setOnClickListener(this);
-        bstop.setOnClickListener(this);
+        //bstart.setOnClickListener(this);
+        //bstop.setOnClickListener(this);
 
-        Intent serviceToggle = new Intent(MainActivity.this, RingService.class);
+        bstart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp = MediaPlayer.create(MainActivity.this, Settings.System.DEFAULT_RINGTONE_URI );
+                mp.start();
+            }
+        });
+
+        bstop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp.stop();
+            }
+        });
+
+
+
+
+        //Intent serviceToggle = new Intent(MainActivity.this, RingService.class);
 
 
         new Handler().postDelayed(new Runnable() {
@@ -42,14 +83,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
+    private boolean isMyServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    protected void onDestroy() {
+        stopService(mServiceIntent);
+        super.onDestroy();
+
+    }
+
+
+    /*
     @Override
     public void onClick(View view) {
         if(view == bstart) {
-            startService(new Intent(this, RingService.class));
+            mp.start();
         } else if (view == bstop) {
-            stopService(new Intent(this, RingService.class));
+            mp.stop();
+            mp.reset();
         }
-    }
+    } */
 
 
 }
--- app/src/main/java/com/example/askok/ringerservice/RingService.java
+++ app/src/main/java/com/example/askok/ringerservice/RingService.java
@@ -1,13 +1,93 @@
 package com.example.askok.ringerservice;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.IBinder;
 import android.provider.Settings;
 import android.support.annotation.Nullable;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class RingService extends Service {
+
+    //public MediaPlayer mp = MediaPlayer.create(this, Settings.System.DEFAULT_RINGTONE_URI);
+
+    public int counter=0;
+    public RingService(Context applicationContext) {
+        super();
+    }
+
+    public RingService() {
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+        startTimer();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Intent broadcastIntent = new Intent(this, SensorRestarterBroadcastReceiver.class);
+        sendBroadcast(broadcastIntent);
+        stoptimertask();
+    }
+
+    private Timer timer;
+    private TimerTask timerTask;
+    long oldTime=0;
+    public void startTimer() {
+        //set a new Timer
+        timer = new Timer();
+
+        //initialize the TimerTask's job
+        initializeTimerTask();
+
+        //schedule the timer, to wake up every 1 second
+        timer.schedule(timerTask, 1000, 1000); //
+    }
+
+    /**
+     * it sets the timer to print the counter every x seconds
+     */
+    public void initializeTimerTask() {
+        timerTask = new TimerTask() {
+            public void run() {
+            }
+        };
+    }
+
+    public void stoptimertask() {
+        //stop the timer, if it's not already null
+        if (timer != null) {
+            timer.cancel();
+            timer = null;
+        }
+    }
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    /*
+    public void startPlaying() {
+        mp.start();
+    }
+
+    public void stopPlaying() {
+        mp.stop();
+    }
+*/
+
+
+    /*
 
     private MediaPlayer mp;
 
@@ -32,7 +112,6 @@ public class RingService extends Service {
     public void onDestroy() {
         super.onDestroy();
         mp.stop();
-    }
-
+    } */
 
 }
